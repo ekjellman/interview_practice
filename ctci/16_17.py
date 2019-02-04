324bce3258a02ede3128a00a87700c76180ec16b
Eric Kjellman <ekjellman@gmail.com>
2019-02-04 11:47:48 -0800

    a couple more

---
+++ ctci/16_17.py
@@ -0,0 +1,38 @@
+###
+# Problem
+###
+
+# Given a list of numbers, positive and negative, find the contiguous sequence
+# with the largest sum and output the sum.
+
+###
+# Work
+###
+# Questions:
+# Size of n?
+# What if there are no numbers? (Assume there are numbers, although 0 is fine)
+# What if there are all negative numbers? (Return 0, for an empty sequence)
+
+def lcs(numbers):
+  best = 0
+  current = 0
+  for number in numbers:
+    current += number
+    if current < 0:
+      if current - number > best:
+        best = current - number
+      current = 0
+  return max(best, current)
+
+# Tests
+print lcs((2, -8, 3, -2, 4, -10)), "5"
+print lcs((-1, -2, -3, -4)), "0"
+print lcs(()), "0"
+print lcs((-1, 2, 2, 2)), "6"
+
+# Time: 5 minutes
+
+###
+# Mistakes / Bugs / Misses
+###
+# none
---
+++ ctci/16_18.py
@@ -0,0 +1,58 @@
+###
+# Problem
+###
+
+# You have two strings, pattern and value. pattern consists of letters a and b
+# describing a patter within a string. For example "catcatgocatgo" matches
+# "aabab" with a = "cat" and b = "go". It also matches other patterns.
+# Write a method to determine if value matches pattern.
+
+###
+# Work
+###
+# Questions:
+# Can a and b be empty? (Assuming no)
+# Case, etc (assume cast sensitive)
+# Extensible?
+
+def matches(pattern, value, pattern_map=None):
+  if not pattern: return len(value) == 0
+  if pattern_map is None:
+    pattern_map = {}
+  letter = pattern[0]
+  if letter in pattern_map:
+    if value.startswith(pattern_map[letter]):
+      return matches(pattern[1:], value[len(pattern_map[letter]):], pattern_map)
+    else:
+      return False
+  else:
+    for length in xrange(1, len(value) + 1):
+      pattern_map[letter] = value[:length]
+      result = matches(pattern[1:], value[len(pattern_map[letter]):], pattern_map)
+      del pattern_map[letter]
+      if result:
+        return True
+    return False
+
+# Tests
+print "All true"
+print matches("aabab", "catcatgocatgo")
+print matches("bbaba", "catcatgocatgo")
+print matches("a", "catcatgocatgo")
+print matches("ab", "catcatgocatgo")
+print matches("a", "catcatgocatgo")
+print "All false"
+print matches("aaa", "catcatgocatgo")
+print matches("aaaa", "catcatcat")
+print matches("aaaa", "catcatcatcatcat")
+
+# Time: 15 minutes
+
+###
+# Mistakes / Bugs / Misses
+###
+# Off by one error at 29. only went to len(value). I need len(value) + 1 to
+#   get the whole string (TODO: Make card)
+# There are probably some improvements if all letters in pattern are known
+# Thought about but did not implement the length optimization for the a and b
+#   only case, since I made it more generalized than that.
